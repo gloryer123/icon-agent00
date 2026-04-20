d83ca89cc71ca4de0e8532aa9ea57f818ba45b49
gloryer123 <1944560018@qq.com>
2026-04-20 11:57:50 +0800

    1.连接API，按需求选择学校

---
+++ src/main/java/com/wzj/agent00/Agent00Application.java
@@ -0,0 +1,13 @@
+package com.wzj.agent00;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Agent00Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Agent00Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/wzj/agent00/Agent00ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wzj.agent00;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Agent00ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
